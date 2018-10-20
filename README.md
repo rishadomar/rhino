4b2f863811ae3d33dbbdcf1f1cafc71988c228be
Rishad Omar <rishadomar@gmail.com>
2018-10-20 12:22:13 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+cd ~/src/laravel/rhino
+php artisan serve
+
+(on browser: http://127.0.0.1:8000)
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('content')
+<p>additional</p>
+@endsection
+
+@section('title')
+    Contact Us
+@endsection
